d6d1c283783cf864753df1add1744fa3df4a3c80
Schalk Burger <schalkb@gmail.com>
2020-12-16 16:47:01 +0200

    Add reset links back to home

--- enhanced-fnb.user.js
+++ enhanced-fnb.user.js
@@ -1,140 +1,153 @@
 @-moz-document domain("fnb.co.za") {
-   /* Hide elements */
-
+    /* Hide elements */
     body > div.section.height2.headerTabs.backTurq,
     body > div.section.height2.headerTabs.backTurq > div,
-    div.loginFields ~ div.wrapperFloatRight,
     .slide,
     .sectionFixedFooterButton,
     .sideNote.legal,
     .CovidWrapper,
     .searchButton,
     #actionMenuBtt {
-    display: none;
+        display: none;
     }
-
+    
     /* Global */
-
     .backTurq,
     .overlay .section .section.backTurq {
-    background: #00999b;
+        background: #00999b;
     }
-
+    
     /* Header */
-
     .header {
-    position: relative;
-    top: 0;
-    height: 100% !important;
+        position: relative;
+        top: 0;
+        height: 100% !important;
     }
-
+    
     .homepageLink img {
-    max-height: 84px;
+        max-height: 84px;
     }
-
+    
     /* Landing */
-
     .homepageLink img {
-    margin: 20px auto;
+        margin: 20px auto;
     }
-
+    
     .sectionColFull {
-    display: flex;
-    justify-content: center;
+        display: flex;
+        justify-content: center;
     }
-
+    
     .sectionColFull .sectionColInner {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
     }
-
+    
     .wrapperHeight100 {
-    height: auto;
-    display: flex;
-    justify-content: center;
+        height: auto;
+        display: flex;
+        justify-content: center;
     }
-
+    
     .vAlignMiddle {
-    top: 0;
-    transform: none;
+        top: 0;
+        transform: none;
     }
-
+    
     /* Login Form */
-
     .loginFields {
-    display: flex;
-    flex-direction: column;
-    float: none;
-    top: 0;
-    transform: none;
+        display: flex;
+        flex-direction: column;
+        float: none;
+        top: 0;
+        transform: none;
     }
-
+    
     .loginFields .textinput {
-    width: 100%;
+        width: 100%;
     }
-
-    input[type="text"],
-    input[type="password"],
-    input[type="submit"],
-    input[type="button"] {
-    height: auto;
-    padding: 15px 20px;
-    width: 100%;
-    margin: 4px 0;
-    background: #fff;
-    font-size: 18px;
+    
+    input[type=text],
+    input[type=password],
+    input[type=submit],
+    input[type=button] {
+        height: auto;
+        padding: 15px 20px;
+        width: 100%;
+        margin: 4px 0;
+        background: #fff;
+        font-size: 18px;
     }
-
-    input[type="submit"],
-    input[type="button"] {
-    border-color: #fa9d1e;
-    background-color: #fa9d1e;
-    padding: 10px;
+    
+    input[type=submit],
+    input[type=button] {
+    
+        border-color: #fa9d1e;
+        background-color: #fa9d1e;
+        padding: 10px;
     }
-
-    input[type="submit"]:hover,
-    input[type="button"]:hover {
-    background: #d88515;
-    color: #fff;
+    
+    input[type=submit]:hover,
+    input[type=button]:hover {
+        background: #d88515;
+        color: #fff;
     }
-
-    input[type="text"]::-webkit-input-placeholder,
+    
+    input[type=text]::-webkit-input-placeholder,
     input,
-    input[type="password"]::-webkit-input-placeholder {
-    font-size: 18px;
+    input[type=password]::-webkit-input-placeholder {
+        font-size: 18px;
     }
-
+    
     .loginFields .inputButtonHolder {
-    float: none;
-    height: auto;
+        float: none;
+        height: auto;
     }
-
+    
+    .loginFields > div {
+        display: inline-block;
+    }
+    
+    form#LOGIN_FORM {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        align-items: center;
+    }
+    
+    .inputButtonHolder {
+        margin-bottom: 20px;
+    }
+    
+    .link.fontWhite {
+        line-height: 2;
+        font-size: 14px;
+    }
+    
     /* Main */
-
     #bodyContainer .bankingLayoutTwoInner {
-    display: none;
+        display: none;
     }
-
+    
     #bodyContainer .iconButtonGroup ul.alignIconsCenter {
-    display: flex;
-    flex-wrap: wrap;
-    margin: 20px auto;
-    max-width: 1050px;
+        display: flex;
+        flex-wrap: wrap;
+        margin: 20px auto;
+        max-width: 1050px;
     }
-
+    
     #bodyContainer .iconButtonGroup.big .ui-icon {
-    width: 150px;
-    height: 150px;
-    background-size: 150px;
+        width: 150px;
+        height: 150px;
+        background-size: 150px;
     }
-
+    
     #bodyContainer #welcomeContainer {
-    margin: 20px 0 10px 0;
+        margin: 20px 0 10px 0;
     }
-
+    
     #bodyContainer #activityContainer p {
-    line-height: 2;
+        line-height: 1.5;
     }
-
-}
+    }
